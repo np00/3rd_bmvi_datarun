0458e9e953bbddda79f4c1efea33215a01b2e8cd
Niklas Petersen <niklas.petersen@openmailbox.org>
2018-03-03 14:23:26 +0100

    final version

--- js/mapper.js
+++ js/mapper.js
@@ -1,7 +1,7 @@
 // hard coded :'(
 
 var line_ice, line_tram_66, animated_ice, animated_tram
-
+var markers
 
 function init_station()
 {
@@ -12,11 +12,13 @@ function init_station()
       var dusseldorf_hbf = [51.2195845, 6.7949628] ; 
       var siegburg = [50.671195, 7.429848] ;
       var before_siegburg = [50.692731, 7.390022] ;
+      var bonn_hbf = [50.7320436, 7.0967647] ;
 
       // Tram 66 
       var siegburg = [50.7929167, 7.2029256]; 
       var hangelar_ost = [50.7637489, 7.1717841] ; 
       var sankt_augustin = [50.7768318, 7.1880754] ;
+
 
 
     mannheim_marker = L.marker(mannheim_hbf, {icon:icon_train_station}).addTo(map)
@@ -41,10 +43,13 @@ function init_station()
                 Mobiler Service: <b>08:00 - 20:00</b> <br> 
                 ...
                 <br> 
-                 <button type="button">Zeige Restaurants!</button> 
+                    <button type="button" onclick="executeLgdQuery();">Suche Kneipen!</button> 
                   `);
     hangelar_ost_marker = L.marker(hangelar_ost, {icon:icon_tram_station}).addTo(map)
     .bindPopup("<h1>Hangelar Ost</h1>");
+
+    bonn_marker = L.marker(bonn_hbf, {icon:icon_tram_station}).addTo(map)
+    .bindPopup("<h1>Bonn Hbf</h1>");
   
     sankt_augustin_marker = L.marker(sankt_augustin, {icon:icon_tram_station}).addTo(map)
     .bindPopup("<h1>Sankt Augustin</h1>");
@@ -52,7 +57,52 @@ function init_station()
   line_ice = L.polyline([before_siegburg, before_siegburg, siegburg]);
   line_tram_66_to_siegburg = L.polyline([hangelar_ost, hangelar_ost, sankt_augustin, siegburg]);
   line_tram_66_from_siegburg = L.polyline([siegburg, siegburg, sankt_augustin, hangelar_ost]);
+  
+  /*line_bonn = L.polyline([
+[50.7942173,7.2060013],
+[50.7892256,7.2022247],
+[50.7846676,7.1957016],
+[50.7801091,7.1915817],
+[50.7751159,7.1871185],
+[50.7694708,7.1809387],
+[50.7659966,7.1764755],
+[50.759916,7.1740723],
+[50.7566583,7.1678925],
+[50.7536175,7.1603394],
+[50.752097,7.1490097],
+[50.7514454,7.1400833],
+[50.7503593,7.1315002],
+[50.7471009,7.1253204],
+[50.743625,7.1225739],
+[50.7410179,7.1191406],
+[50.7388452,7.1146774],
+[50.7375415,7.1057510],
+[50.7366724,7.0957947],
+[50.7336303,7.0951080],
+[50.7375415,7.0954514],
+[50.7375415,7.1019745],
+[50.7377588,7.1112442],
+[50.739497,7.1181107],
+[50.7425387,7.1236038],
+[50.7471009,7.1270370],
+[50.7492732,7.1328735],
+[50.7501421,7.1397400],
+[50.7512282,7.1507263],
+[50.7516626,7.1579361],
+[50.7542691,7.1644592],
+[50.757527,7.1696091],
+[50.7610019,7.1764755],
+[50.7659966,7.1775055],
+[50.7701222,7.1819687],
+[50.773379,7.1867752],
+[50.7785895,7.1895218],
+[50.7818457,7.1953583],
+[50.7855358,7.1981049],
+[50.7881404,7.2029114],
+[50.7922641,7.2060013],
+[50.7963874,7.2053146] ]);
      
+     */
 
   animated_ice = L.animatedMarker(line_ice.getLatLngs(), {
     icon: icon_ice,
@@ -68,12 +118,23 @@ function init_station()
 
   });
 
+/*
+animated_tram_from_bonn = L.animatedMarker(line_bonn.getLatLngs(), {
+    icon: icon_tram,
+    distance: 500,  // meters
+    interval: 1000, // milliseconds
+  });
+
+*/
+
   animated_tram_from_siegburg = L.animatedMarker(line_tram_66_from_siegburg.getLatLngs(), {
     icon: icon_tram,
     distance: 500,  // meters
     interval: 1000, // milliseconds
+    onEnd: function() {
+       atom_bomb_marker = L.marker(hangelar_ost, {icon:atom_bomb}).addTo(map)
+  }});
 
-  });
 
     var route_ice = L.featureGroup([
       mannheim_marker,
@@ -95,20 +156,64 @@ function init_station()
       siegburg_marker,
     ]);
 
+/*
+
+  var route_bonn = L.featureGroup([
+      bonn_marker,
+      L.polyline([
+[50.7942173,7.2060013],
+[50.7892256,7.2022247],
+[50.7846676,7.1957016],
+[50.7801091,7.1915817],
+[50.7751159,7.1871185],
+[50.7694708,7.1809387],
+[50.7659966,7.1764755],
+[50.759916,7.1740723],
+[50.7566583,7.1678925],
+[50.7536175,7.1603394],
+[50.752097,7.1490097],
+[50.7514454,7.1400833],
+[50.7503593,7.1315002],
+[50.7471009,7.1253204],
+[50.743625,7.1225739],
+[50.7410179,7.1191406],
+[50.7388452,7.1146774],
+[50.7375415,7.1057510],
+[50.7366724,7.0957947],
+[50.7336303,7.0951080],
+[50.7375415,7.0954514],
+[50.7375415,7.1019745],
+[50.7377588,7.1112442],
+[50.739497,7.1181107],
+[50.7425387,7.1236038],
+[50.7471009,7.1270370],
+[50.7492732,7.1328735],
+[50.7501421,7.1397400],
+[50.7512282,7.1507263],
+[50.7516626,7.1579361],
+[50.7542691,7.1644592],
+[50.757527,7.1696091],
+[50.7610019,7.1764755],
+[50.7659966,7.1775055],
+[50.7701222,7.1819687],
+[50.773379,7.1867752],
+[50.7785895,7.1895218],
+[50.7818457,7.1953583],
+[50.7855358,7.1981049],
+[50.7881404,7.2029114],
+[50.7922641,7.2060013],
+[50.7963874,7.2053146]
+        ]),
+      siegburg_marker,
+    ]);
+    */
 
   //map.fitBounds(route.getBounds());
 
 
     map.addLayer(route_ice);
     map.addLayer(route_tram);
-
-    function snake() {
-      route.snakeIn();
-    }
-
-    route.on('snakestart snake snakeend', function(ev){
-      console.log(ev.type);
-    });
+   // map.addLayer(route_bonn);
 }
 
 
@@ -116,6 +221,12 @@ function start_ice()
 { 
    map.addLayer(animated_ice);
    animated_ice.start();
+}
+
+function start_tram_bonn()
+{ 
+   map.addLayer(animated_tram_from_bonn);
+   animated_tram_from_bonn.start();
 }
 
 
@@ -132,16 +243,6 @@ function start_tram_from_siegburg()
    map.addLayer(animated_tram_from_siegburg);
    animated_tram_from_siegburg.start()
 }
-
-
-
-
-
-
-
-
-
-
 
 
     //snake()
@@ -228,23 +329,11 @@ function processData(results)
 }
 
 
-
-// Linked Geo Data Query Execution 
-function executeLgdQuery() 
-{
-  // construct http get request 
-  full_query = encodeURIComponent(query_city_bonn)
-  
-  // execute request 
-  httpGetAsync(full_query, processData)
-}
-
-
 // Linked Geo Data Query Execution 
 function executeLgdQuery() 
 {
   // read query from HTML Text area
-  lgd_query = lgd_editor.getValue()
+  lgd_query = lgd_default_query
 
   // construct http get request 
   full_query = lgd_graph_uri + encodeURIComponent(lgd_query) + lgd_result_format 
@@ -311,7 +400,11 @@ function drawToMap(instanceName, instanceGeo) {
   long = long.slice(0, long.length-1)
 
 
-  new_marker = L.marker([long, lat], {icon:markerIconRed}).addTo(map)
+  console.log(lat)
+  console.log(long)
+  console.log(instanceName)
+
+  new_marker = L.marker([long, lat], {icon:beerIcon}).addTo(map)
     .bindPopup(instanceName)
     .openPopup();
 
@@ -331,9 +424,22 @@ var markerIcon = L.icon({
     iconSize: [20, 20], 
 });
 
+
+var beerIcon = L.icon({
+    iconUrl: 'img/beer.png',
+    iconSize: [20, 20], 
+});
+
 var icon_train_station = L.icon({
     iconUrl: 'img/train_station.png',
     iconSize: [20, 20], 
+});
+
+
+
+var atom_bomb = L.icon({
+    iconUrl: 'https://media.giphy.com/media/oe33xf3B50fsc/giphy.gif',
+    iconSize: [200, 200], 
 });
 
 
@@ -384,6 +490,8 @@ function onMapClick(e) {
 
 map = L.map('map').setView([50.7674, 7.2156], 12);
 
+var markers = L.featureGroup();
+map.addLayer(markers)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -411,14 +519,14 @@ PREFIX owl: <http://www.w3.org/2002/07/owl#>
 
 SELECT ?name, ?geo 
 {
-    ?bonn owl:sameAs <http://dbpedia.org/resource/Bonn> .
+    ?bonn owl:sameAs <http://dbpedia.org/resource/Siegburg> .
     ?bonn geom:geometry [ ogc:asWKT ?bonnGeo] .
 
     ?bar a lgd:Bar .
     ?bar rdfs:label ?name .    
     ?bar geom:geometry [ ogc:asWKT ?geo] .
 
-    FILTER(bif:st_intersects (?bonnGeo, ?geo, 5)) .
+    FILTER(bif:st_intersects (?bonnGeo, ?geo, 1)) .
 
 } LIMIT 10`
 lgd_result_format = "&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on"
